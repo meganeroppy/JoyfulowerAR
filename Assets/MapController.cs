a0474cc9e071534303e9edf3665149f8a7684123
SPIKE-CHUNSOFT\w-fukushima <roppy@hotmail.co.jp>
2017-03-22 15:12:52 +0900

    マップ操作スクリプト作成

---
+++ Assets/MapController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapController : MonoBehaviour {
+
+	[SerializeField]
+	OnlineMaps map;
+
+	// Use this for initialization
+	void Start () {
+		map.zoom = 20;
+
+//		map.SetPosition( longitude, latitude );
+
+	
+		OnlineMapsMarker marker;
+	//	marker.
+	//	map.AddMarker()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
